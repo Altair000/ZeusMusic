658d5d12b2bbab93745d5117f595930b5e4ca2b1
Altair000 <raydiel_e@icloud.com>
2024-09-19 21:57:34 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ app = Flask(__name__)
 BOT_TOKEN = os.getenv('Token')
 HEROKU_URL = 'https://zeusmusicbot-610073f53b03.herokuapp.com/'
 
-@app.route(f"/{BOT_TOKEN}", methods=['POST'])
-def receive_updates():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "!", 200
+# Configura el webhook
+HEROKU_APP_NAME = 'zeusmusicbot'  # Asegúrate de definir esto en Heroku
+WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com/{BOT_TOKEN}"
 
-# Establecer el webhook en Telegram
-bot.remove_webhook()
-bot.set_webhook(url=f"{HEROKU_URL}/{BOT_TOKEN}")
+@app.route('/' + BOT_TOKEN, methods=['POST'])
+def webhook():
+    update = request.get_json()
+    bot.process_new_updates([telebot.types.Update.de_json(update)])
+    return 'OK', 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
