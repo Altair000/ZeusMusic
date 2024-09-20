0c8144e847c0810136a0ddf423f9a3b820130e1e
Altair000 <raydiel_e@icloud.com>
2024-09-19 23:49:03 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import telebot
 import os
-from bot.handlers import bot, register_handlers
+import telebot
 from flask import Flask, request
+from bot.handlers import register_handlers  # Asegúrate de importar tus handlers
 
 app = Flask(__name__)
-TOKEN = "7465330159:AAHD3x86Qn5f69ztjR5YrK-VbO11589Bntw"
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-@app.route('/' + TOKEN, methods=['POST'])
+@app.route('/' + TELEGRAM_TOKEN, methods=['POST'])
 def webhook():
     update = request.get_json()
     if update:
@@ -19,5 +20,5 @@ if __name__ == '__main__':
 
     # Configurar el webhook
     bot.remove_webhook()
-    bot.set_webhook(url=f"https://zeusmusicbot-610073f53b03.herokuapp.com/{TOKEN}")
+    bot.set_webhook(url=f"https://zeusmusicbot.herokuapp.com/{TELEGRAM_TOKEN}")
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
