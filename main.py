5cf393e1d4da341986955a855557fe21ebab8612
Altair000 <raydiel_e@icloud.com>
2024-09-19 21:22:30 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from bot.handlers import bot
+from flask import Flask, request
+
+app = Flask(__name__)
+TOKEN = os.getenv('Token')
+HEROKU_URL = 'https://zeusmusicbot-610073f53b03.herokuapp.com/'
+
+@app.route(f"/{TOKEN}", methods=['POST'])
+def receive_updates():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
+
+# Establecer el webhook en Telegram
+bot.remove_webhook()
+bot.set_webhook(url=f"{HEROKU_URL}/{BOT_TOKEN}")
 
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
