bad68050290ac0856e8ec5d13e43a31d6503ae7d
Altair000 <raydiel_e@icloud.com>
2024-09-19 23:17:15 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import telebot
 import os
-from bot.handlers import bot
+from bot.handlers import bot, register_handlers
 from flask import Flask, request
 
 app = Flask(__name__)
 TOKEN = os.environ.get('Token')
 
-@app.route('/' + TOKEN, methods=['POST', 'GET'])
+@app.route('/' + TOKEN, methods=['POST'])
 def webhook():
-    try:
-        update = request.get_json()
-        if update:
-            bot.process_new_updates([telebot.types.Update.de_json(update)])
-    except Exception as e:
-        print(f"Error: {e}")
+    update = request.get_json()
+    if update:
+        bot.process_new_updates([telebot.types.Update.de_json(update)])
     return 'OK', 200
 
 if __name__ == '__main__':
+    # Registrar los handlers
+    register_handlers(bot)
+
+    # Configurar el webhook
     bot.remove_webhook()
     bot.set_webhook(url=f"https://zeusmusicbot-610073f53b03.herokuapp.com/{TOKEN}")
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
