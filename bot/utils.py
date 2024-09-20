559fd0e7b519e077b1bb79199715caf944be1116
Altair000 <raydiel_e@icloud.com>
2024-09-20 10:50:48 -0400

    Actualizar utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -52,6 +52,7 @@ def download_music(video_url, quality):
         'noplaylist': True,
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
         'nocheckcertificate': True,
+        'verbose': True,
         'cookies': 'cookies.txt'
     }
 
