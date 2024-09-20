d2e0d9fc2aea7e78402f4a4846e3b0952d4edfe7
Altair000 <raydiel_e@icloud.com>
2024-09-19 23:00:46 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from flask import Flask, request
 app = Flask(__name__)
 TOKEN = os.environ.get('Token')
 
-@app.route('/' + TOKEN, methods=['GET'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "¡Mensaje recibido!", 200
-
-@app.route("/" + TOKEN, methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://zeusmusicbot-610073f53b03.herokuapp.com/' + TOKEN)
-    return "¡Webhook configurado!", 200
+    try:
+        update = request.get_json()
+        if update:
+            bot.process_new_updates([telebot.types.Update.de_json(update)])
+    except Exception as e:
+        print(f"Error: {e}")
+    return 'OK', 200
 
-# Main
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://zeusmusicbot-610073f53b03.herokuapp.com/{TOKEN}")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
